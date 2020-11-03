98d5e08d1250bf0205ca2711e7e4abe51a9d65e7
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-11-03 15:49:49 -0500

    adding some data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+//going to use this css
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//our todo list that holds our data, hard coded
+export default class App extends Component{
+
+  state={
+    todoList:[
+      {
+        id: 1,
+        todo: "Hang up stockings"
+      },
+      {
+        id: 2,
+        todo: "Decorate Tree",
+      },
+      {
+        id:3,
+        todo:"Bake Cookies"
+        
+      },
+      {
+        id:"",
+        todo:""
+      }
+  
+    ]
+  }
 }
 
-export default App;
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
